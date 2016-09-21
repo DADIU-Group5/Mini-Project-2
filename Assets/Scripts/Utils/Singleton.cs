6fdbac5efd6303e1be07ee36f1165214f636b8cf
Bjørn <g.bvinther@gmail.com>
2016-09-21 11:08:18 +0200

    Initial setup

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+namespace Mini2.Utils
+{
+    /// <summary>
+    /// Manager scripts should extend this class to implement a singleton pattern
+    /// </summary>
+    /// <typeparam name="T">Type of class</typeparam>
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        public static T instance;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+            }
+            else
+            {
+                Debug.Log("Duplicate singetons of type: " + typeof(T));
+            }
+        }
+    }
+}
