fcef6e9e26b561f2b0871d152ac84bd1e59b7527
Camilla <camilla.g.jakobsen@hotmail.com>
2016-09-28 13:05:33 +0200

    score modify added to enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,10 +10,14 @@ public class Enemy : MonoBehaviour
 {
     public int lane;
     public EnemyType enemyType;
+    [Range(1, 10f)]
+    [Tooltip("Score points gained/lost from enemy")]
+    public int enemyPoints = 1;
 
     public void DestroySelf()
     {
         EnemyManager.instance.RemoveEnemyFromLane(lane);
+        ScoreManager.instance.ModifyPoint(enemyPoints);
         Destroy(this.gameObject);
     }
 
