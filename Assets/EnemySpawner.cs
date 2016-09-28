db4ebb33bb75dbabe7767f2a37d1642ad749f2e5
Frederik <fknudsen@gmail.com>
2016-09-28 12:26:41 +0200

    Made prefabs for the 3 different enemy types.

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -4,12 +4,27 @@ using Mini2.Utils;
 
 public class EnemySpawner : Singleton<EnemySpawner>{
 
-    public GameObject enemyObj;
+    public GameObject pirateObj;
+    public GameObject mayanObj;
+    public GameObject spaceObj;
     
     public void SpawnEnemy(float pos, EmptyEnemy Ee, EnemyType type)
     {
         int lane = GetLane();
-        GameObject go = Instantiate(enemyObj, new Vector3(pos, 0, lane-1), Quaternion.identity) as GameObject;
+        GameObject toSpawn;
+        if(type == EnemyType.Mayan)
+        {
+            toSpawn = mayanObj;
+        }
+        else if(type == EnemyType.Pirate)
+        {
+            toSpawn = pirateObj;
+        }
+        else
+        {
+            toSpawn = spaceObj;
+        }
+        GameObject go = Instantiate(toSpawn, new Vector3(pos, 0, lane-1), Quaternion.identity) as GameObject;
         go.GetComponent<Enemy>().AssignEnemyType(lane, type);
     }
 
