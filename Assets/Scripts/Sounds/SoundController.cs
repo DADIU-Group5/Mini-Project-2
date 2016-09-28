80fde2227fcd496059ccc796d98d94f660b2351c
DADIU <mabiscus@gmail.com>
2016-09-28 13:50:18 +0200

    Scene and script for easily testing if Wwise integration works.

---
+++ Assets/Scripts/Sounds/SoundController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            AudioMaster.instance.PlayEvent("enemyBlolbMove");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            AudioMaster.instance.PlayEvent("enemyBlolbMove", 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            AudioMaster.instance.PlayEvent("enemyBlolbMove", 1452.2f);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            AudioMaster.instance.PlayEvent("enemyBlolbMove", -2f);
+        }
+    }
+}
