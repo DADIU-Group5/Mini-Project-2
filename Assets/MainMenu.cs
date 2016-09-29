66b160f4427880a8253ba9e322117d44bcbd5e2a
Frederik <fknudsen@gmail.com>
2016-09-29 11:03:40 +0200

    Created MainMenu. Created SaveData script for saving stars in each level.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject basicButton;
+
+    public Text playText;
+    public Text changeLanguageText;
+
+    public GameObject main;
+    public GameObject levels;
+
+    public int amountOfLevels = 3;
+
+	// Use this for initialization
+	void Start () {
+        if(SaveData.instance.BeenPlayed(1) == false)
+        {
+            SaveData.instance.SaveStarsForLevel(1, 0);
+        }
+        ShowLevels();
+	}
+
+    public void ShowLevels()
+    {
+        amountOfLevels = SceneManager.sceneCount;
+        for (int i = 0; i < amountOfLevels; i++)
+        {
+            GameObject GO = Instantiate(basicButton, transform) as GameObject;
+            GO.GetComponent<MainMenuButton>().Setup(i + 1, this);
+        }
+    }
+
+    public void Play()
+    {
+        main.SetActive(false);
+    }
+
+    public void ChangeLanguage()
+    {
+        SaveData.instance.ChangeLanguage();
+        if (SaveData.instance.IsLanguageEnglish())
+        {
+            playText.text = "Play!";
+            changeLanguageText.text = "Change Language";
+        }
+        else
+        {
+            playText.text = "Spil!";
+            changeLanguageText.text = "Skift Sprog";
+        }
+    }
+	
+	public void LoadLevel(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
---
+++ Assets/SaveData.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mini2.Utils;
+
+public class SaveData : Singleton<SaveData> {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+
+    public bool BeenPlayed(int level)
+    {
+        return PlayerPrefs.HasKey("LevelStars" + level);
+    }
+	
+	public int GetStarsForLevel(int level)
+    {
+        return PlayerPrefs.GetInt("LevelStars" + level);
+    }
+
+    public void SaveStarsForLevel(int level, int stars)
+    {
+        PlayerPrefs.SetInt("LevelStars" + level, stars);
+    }
+
+    public void SaveStarsForCurrentLevel(int stars)
+    {
+        PlayerPrefs.SetInt("LevelStars" + UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex, stars);
+    }
+
+    public void CompletedCurrentLevel()
+    {
+        PlayerPrefs.SetInt("LevelStars" + UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex+1, 0);
+    }
+
+    public bool IsLanguageEnglish()
+    {
+        if(PlayerPrefs.GetInt("English") == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void ChangeLanguage()
+    {
+        if(PlayerPrefs.GetInt("English") == 0)
+        {
+            PlayerPrefs.SetInt("English", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("English", 0);
+        }
+    }
+}
