49a815701bbbc590fed0533b08253a0ba8895d6d
Mia <mia@sogaard.us>
2016-09-29 16:02:23 +0200

    Adjusting star placements (still working on it)

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -23,10 +23,7 @@ public class UIController : Singleton<UIController> {
     void Start()
     {
         starImages = new GameObject[3];
-        //disable the image component of all the star images
-        star1.GetComponent<Image>().enabled = false;
-        star2.GetComponent<Image>().enabled = false;
-        star3.GetComponent<Image>().enabled = false;
+        DisableStars();
         starImages[0] = star1;
         starImages[1] = star2;
         starImages[2] = star3;
@@ -44,13 +41,20 @@ public class UIController : Singleton<UIController> {
         StarSystem.instance.CalculateScore();
         int stars = StarSystem.instance.starRating;
         if (stars != 0) {
+            float range = Mathf.Abs (star3.transform.position.x+2) - Mathf.Abs (star1.transform.position.x+2);
+            Debug.Log("the range is: " + range + " with star 1 at : " + star1.transform.position.x + " with star 3 at : " + star3.transform.position.x);
+            
             for (int i = 0; i < Mathf.Abs(stars); i++)
             {
+                float point = (range / (stars+1)) * (i + 1);
+                Debug.Log(range + " / " + (stars + 1) + " * " + (i + 1) + " = " + point);
                 starImages[i].gameObject.GetComponent<Image>().enabled = true;
                 if (stars < 0)
                 {
                     starImages[i].gameObject.GetComponent<Image>().color = Color.red;
                 }
+                starImages[i].transform.position = new Vector3(star1.transform.position.x + point, starImages[i].transform.position.y, starImages[i].transform.position.z);
+                Debug.Log("Star " + (i+1) + " at star1.transform.position.x + point: " + (star1.transform.position.x + point));
             }
         }
         SaveData.instance.CompletedCurrentLevel();
@@ -81,11 +85,13 @@ public class UIController : Singleton<UIController> {
 
     public void MainMenu()
     {
+        DisableStars();
         SceneManager.LoadScene(0);
     }
 
     public void NextLevel()
     {
+        DisableStars();
         if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings-1)
         {
             SceneManager.LoadScene(0);
@@ -98,6 +104,15 @@ public class UIController : Singleton<UIController> {
 
     public void RetryLevel()
     {
+        DisableStars();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void DisableStars()
+    {
+        //disable the image component of all the star images
+        star1.GetComponent<Image>().enabled = false;
+        star2.GetComponent<Image>().enabled = false;
+        star3.GetComponent<Image>().enabled = false;
     }
 }
