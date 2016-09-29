976ff87873ca6dd1f5c4c5a97929d8e1a4be6495
Camilla <camilla.g.jakobsen@hotmail.com>
2016-09-29 14:54:25 +0200

    temp player models + animations, model change

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -17,8 +17,16 @@ public class Player : MonoBehaviour
     public PlayerState state;
     public GameObject formCube;
 
+    public GameObject pirateModel;
+    public GameObject mayanModel;
+    public GameObject spacemanModel;
+
+    private Animator playerAnimator;
+
     void Start()
     {
+        playerAnimator = this.GetComponentInChildren<Animator>();
+
         ChangeForm();
         jumpStartTime = -jumpTime;
     }
@@ -26,9 +34,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         // Perform jump.
-        // Terrible code, please fix.
-        if (jumpStartTime + jumpTime >= Time.timeSinceLevelLoad)
+        if (jumpStartTime + jumpTime >= Time.timeSinceLevelLoad && jumpStartTime + jumpTime != 0)
         {
+            //playerAnimator.SetTrigger("Jump");
+            playerAnimator.SetBool("Jump", true);
             float t = (Time.timeSinceLevelLoad - jumpStartTime) / jumpTime;
 
             Vector3 pos = transform.position;
@@ -36,6 +45,7 @@ public class Player : MonoBehaviour
         }
         else
         {
+            playerAnimator.SetBool("Jump", false);
             Vector3 pos = transform.position;
             transform.position = new Vector3(pos.x, 0, pos.z);
         }
@@ -71,17 +81,28 @@ public class Player : MonoBehaviour
     public void ChangeForm()
     {
         Debug.Log("Changing form");
+
+        
         if (state.form == 0)
         {
             formCube.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            pirateModel.SetActive(true);
+            mayanModel.SetActive(false);
+            spacemanModel.SetActive(false);
         }
         else if ((int)state.form == 1)
         {
             formCube.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            pirateModel.SetActive(false);
+            mayanModel.SetActive(true);
+            spacemanModel.SetActive(false);
         }
         else if ((int)state.form == 2)
         {
             formCube.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            pirateModel.SetActive(false);
+            mayanModel.SetActive(false);
+            spacemanModel.SetActive(true);
         }
     }
 
