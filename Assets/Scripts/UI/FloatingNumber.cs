b771444ccc346e45a2615344bcc0b9dabb20c8fa
Bjørn <email@email.com>
2016-09-29 11:13:40 +0200

    first iteration of floating numbers.

---
+++ Assets/Scripts/UI/FloatingNumber.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingNumber : MonoBehaviour {
+
+    private const float duration = 2f; 
+    private float destroyTime; // time to destroy object
+
+	// Use this for initialization
+	void Start () {
+        destroyTime = Time.time + duration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Time.time > destroyTime)
+        {
+            Destroy(this);
+        }
+	}
+}
---
+++ Assets/Scripts/UI/FloatingScoreGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FloatingScoreGenerator : MonoBehaviour {
+
+    private GameObject numberPrefab;
+    private GameObject parent;
+
+    void Start()
+    {
+        parent = GameObject.Find("HUD");
+        numberPrefab = Resources.Load<GameObject>("Assets/Prefabs/FloatingNumber.prefab");
+    }
+
+    public void CreateFloatNumber(int number)
+    {
+        var numberString = number < 0 ? "-" + number : "+" + number;
+
+        var screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        GameObject floatingNumber = Instantiate(numberPrefab);
+        floatingNumber.GetComponent<Text>().text = numberString;
+        floatingNumber.GetComponent<RectTransform>().position = screenPos;
+    }
+}
