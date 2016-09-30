edcfdba4924d1ab23793a3e74cf65345c3795fd6
Frederik <fknudsen@gmail.com>
2016-09-30 10:52:08 +0200

    Made things for praveen and tutorial

---
+++ Assets/PauseChunk.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseChunk : TerrainMovement {
+
+    public GameObject pauseUI;
+
+    public string danishText;
+    public string englishText;
+    public waitForSpecificSwipe stopMethod;
+
+    public override void Remove()
+    {
+        if (!SaveData.instance.ShowTutorial())
+        {
+            base.Remove();
+            return;
+        }
+        GameObject.FindObjectOfType<TerrainGenerator>().PauseAllMovement();
+        GameObject go = Instantiate(pauseUI) as GameObject;
+        if (SaveData.instance.IsLanguageEnglish())
+        {
+            go.GetComponent<TutorialUI>().Setup(englishText,stopMethod);
+        }
+        else
+        {
+            go.GetComponent<TutorialUI>().Setup(danishText,stopMethod);
+        }
+        base.Remove();
+    }
+}
+
+public enum waitForSpecificSwipe
+{
+    none, swipeDown, swipeUp, swipeLeft, swipeRight
+}
---
+++ Assets/TutorialUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialUI : MonoBehaviour {
+
+    public Text UItext;
+
+	public void Setup(string t, waitForSpecificSwipe stop)
+    {
+        UItext.text = t;
+        GameObject.FindObjectOfType<InputManager>().SetWaitingForInput(Unpause, stop);
+    }
+
+    public void Unpause()
+    {
+        GameObject.FindObjectOfType<TerrainGenerator>().ResumeAllMovement();
+        Destroy(gameObject);
+    }
+}
