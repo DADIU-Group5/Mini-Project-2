0c2857e9f6d20a5fef0a295d7843a55a6d99849f
Mia <mia@sogaard.us>
2016-09-30 12:34:19 +0200

    Trying to enable death animations at correct times. I seem to have some trouble with the Mayan form.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -14,44 +14,56 @@ public class Enemy : MonoBehaviour
     [Range(1, 10f)]
     [Tooltip("Score points gained/lost from enemy")]
     public int enemyPoints = 1;
-    private Animator enemyAnimator;
-
-    void Start()
-    {
-        enemyAnimator = gameObject.GetComponent<Animator>();
-    }
+    public Animator enemyAnimator;
+    bool test = false;
+    bool pointsGiven = false;
 
     void Update()
     {
         // Die
-        if (hitByPlayer || ScoreManager.instance.score >= 1)
+        
+        if (hitByPlayer && enemyType == GameObject.FindObjectOfType<Player>().state.form)//and enemyType == player.enemyType
         {
+            //Debug.Log("Enabling animator!");
             //Enable animator:
             enemyAnimator.enabled = true;
+            test = true;
         }
+
     }
 
-    public void DestroySelf()
+    public void DestroySelf()//is being called continuosly...
     {
+        StartCoroutine("WaitingCoroutine");
+    }
+
+    IEnumerator WaitingCoroutine()
+    {
+        yield return new WaitForSeconds(1);
         EnemyManager.instance.RemoveEnemyFromLane(lane);
         Destroy(gameObject);
     }
 
     public void DestroyedByPlayer()
     {
-        ScoreManager.instance.ModifyPoint(0, true); //0: enemy
-        if(enemyType == EnemyType.Pirate)
+        if (!pointsGiven)
         {
-            AudioMaster.instance.PlayEvent("enemySharkDeath");
-        }else if (enemyType == EnemyType.Mayan)
-        {
-            AudioMaster.instance.PlayEvent("enemySkeletonDeath");
-        }
-        else if (enemyType == EnemyType.Spaceman)
-        {
-            AudioMaster.instance.PlayEvent("enemyBlobDeath");
-        }
+            ScoreManager.instance.ModifyPoint(0, true); //0: enemy
+            if (enemyType == EnemyType.Pirate)
+            {
+                AudioMaster.instance.PlayEvent("enemySharkDeath");
+            }
+            else if (enemyType == EnemyType.Mayan)
+            {
+                AudioMaster.instance.PlayEvent("enemySkeletonDeath");
+            }
+            else if (enemyType == EnemyType.Spaceman)
+            {
+                AudioMaster.instance.PlayEvent("enemyBlobDeath");
+            }
             DestroySelf();
+            pointsGiven = true;
+        }
     }
 
     public void AssignEnemyType(int _lane, EnemyType _type)
