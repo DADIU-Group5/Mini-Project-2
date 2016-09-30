c6216506dc1f008bfa4e312b612b238a93982d94
Camilla <camilla.g.jakobsen@hotmail.com>
2016-09-30 14:58:33 +0200

    animations on all part of prefab fixed

--- Assets/Scripts/Terrain/Obstacle.cs
+++ Assets/Scripts/Terrain/Obstacle.cs
@@ -8,6 +8,9 @@ public class Obstacle : TerrainMovement {
     public GameObject obstacleModel;
     bool beaten = false;
 
+    private Animation[] mayanAnimations;
+    private Animation[] spaceAnimations;
+
     void Start()
     {
         Obstacles.instance.AddObstacle(this);
@@ -41,12 +44,24 @@ public class Obstacle : TerrainMovement {
         else if (weakAgainst == EnemyType.Mayan)
         {
             AudioMaster.instance.PlayEvent("obstacleSuccesMayan");
-            this.GetComponentInChildren<Animation>().Play();
+            mayanAnimations = GetComponentsInChildren<Animation>();
+            //this.GetComponentInChildren<Animation>().Play();
+            foreach (Animation animation in mayanAnimations)
+            {
+                animation.Play();
+            }
+
         }
         else if (weakAgainst == EnemyType.Spaceman)
         {
             AudioMaster.instance.PlayEvent("obstacleSuccesSpaceman");
-            this.GetComponentInChildren<Animation>().Play();
+            spaceAnimations = GetComponentsInChildren<Animation>();
+            //this.GetComponentInChildren<Animation>().Play();
+            foreach (Animation animation in spaceAnimations)
+            {
+                animation.Play();
+            }
+                
         }
         RemoveObstacle();
     }
